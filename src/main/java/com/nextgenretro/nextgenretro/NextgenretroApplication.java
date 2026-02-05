79a29d2d0ede49187036f0417379d54a288e6230
wellerson <wellerson.pinheiros@outlook.com>
2026-02-04 23:08:45 -0300

    :tada: Commit initial

---
+++ src/main/java/com/nextgenretro/nextgenretro/NextgenretroApplication.java
@@ -0,0 +1,13 @@
+package com.nextgenretro.nextgenretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextgenretroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextgenretroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextgenretro/nextgenretro/NextgenretroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextgenretro.nextgenretro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextgenretroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
